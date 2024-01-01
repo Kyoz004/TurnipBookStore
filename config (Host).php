1040912472540adb1427687f37458161aa9850cd
Kit-DebugCode <136499415+Kyoz004@users.noreply.github.com>
2024-01-01 21:30:36 +0700

    Add files via upload

---
+++ config (Host).php
@@ -0,0 +1,4 @@
+<?php
+// Thiết lập kết nối đến cơ sở dữ liệu
+$conn = mysqli_connect('localhost', 'id21550409_kietdo', 'Spiderman123kkk@', 'id21550409_shop_db') or die('Kết nối thất bại: ' . mysqli_connect_error());
+?>
